d05beb4aa96c70fe4337e56b59597c64f05dfc1c
Renato Sampaio <renatocoral@gmail.com>
2024-12-10 10:12:59 -0300

    Correção do Leitor SHM

--- Memoria_Compartilhada/shm_leitor.c
+++ Memoria_Compartilhada/shm_leitor.c
@@ -6,7 +6,7 @@
 
 int main() {
     // Abre o acesso à área de memória compartilhada como Leitura
-    int shm_fd = shm_open("/shm_example", O_RDONLY, 0666);
+    int shm_fd = shm_open("/shm_exemplo", O_RDONLY, 0666);
 
     // Mapeia a memória para espaço interno do programa
     char *ptr = (char *)mmap(0, 4096, PROT_READ, MAP_SHARED, shm_fd, 0);
@@ -19,6 +19,6 @@ int main() {
     close(shm_fd);
 
     // Remove área compartilhada
-    shm_unlink("/shm_example"); 
+    shm_unlink("/shm_exemplo"); 
     return 0;
 }
