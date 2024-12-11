977d5ac82ccf5e56257f07226d76d21d37e981d5
Renato Sampaio <renatocoral@gmail.com>
2024-12-10 21:46:18 -0300

    Corrige formatação

--- threads/threads_03.c
+++ threads/threads_03.c
@@ -1,30 +1,42 @@
 #include <pthread.h>
 #include <stdio.h>
+
 struct char_print_parms
 {
   char character;
-int count; };
+  int count; 
+};
+
 void* char_print (void* parameters)
 {
   struct char_print_parms* p = (struct char_print_parms*) parameters;
   int i;
-  for (i = 0; i < p->count; ++i)
-  fputc (p->character, stderr);
+  
+  for (i = 0; i < p->count; ++i){
+    fputc (p->character, stderr);
+  }
+
   return NULL;
 }
+
 int main ()
 {
   pthread_t thread1_id;
   pthread_t thread2_id;
+  
   struct char_print_parms thread1_args;
   struct char_print_parms thread2_args;
+  
   thread1_args.character = '1';
   thread1_args.count = 3000;
-    pthread_create (&thread1_id, NULL, &char_print, &thread1_args);
+  pthread_create (&thread1_id, NULL, &char_print, &thread1_args);
+  
   thread2_args.character = '-';
   thread2_args.count = 2000;
   pthread_create (&thread2_id, NULL, &char_print, &thread2_args);
+  
   pthread_join (thread1_id, NULL);
   pthread_join (thread2_id, NULL);
-return 0; 
+  
+  return 0; 
 }
